845757a47222e835e046c048645d53fb19dd6eaf
Mr0Strange1pwn <Pawan.Tiwari@mechlintech.com>
2020-02-28 09:50:19 +0530

    Updates

---
+++ precache-manifest.454b45320d58644f91b71ddb3a96cd14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "744b0b442bd69b0291ec081059076b39",
+    "url": "/newone/index.html"
+  },
+  {
+    "revision": "3b2908c06dd414f50d53",
+    "url": "/newone/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f8f3795d7ea970d22f29",
+    "url": "/newone/static/js/2.d90dc2d7.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/newone/static/js/2.d90dc2d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b2908c06dd414f50d53",
+    "url": "/newone/static/js/main.17cca12e.chunk.js"
+  },
+  {
+    "revision": "872dff3cb6b088a47a31",
+    "url": "/newone/static/js/runtime-main.39063a2e.js"
+  }
+]);
